7f13dcaaf33d7bb6f42742068809c586594522cb
Sebastian Florian <rebardesigndk@gmail.com>
2020-08-03 23:52:13 +0200

    refactor(module): List to Planets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'planets' },
+  { path: 'planets', loadChildren: './modules/planets/planets.module#PlanetsModule' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ListModule } from './modules/list/list.module';
-import { DetailModule } from './modules/detail/detail.module';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-
-    ListModule,
-    DetailModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
