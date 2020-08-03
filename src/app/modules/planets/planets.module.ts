dfc04d66962c37c132bca55956cae16f0e4e4a03
Sebastian Florian <rebardesigndk@gmail.com>
2020-08-04 00:18:33 +0200

    feature(api/Planets): Planets API

--- src/app/modules/planets/planets.module.ts
+++ src/app/modules/planets/planets.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
+import { PlanetsApi } from '../../api/planets.api';
+
 import { PlanetsComponent } from './planets/planets.component';
 import { PlanetComponent } from './planet/planet.component';
 
@@ -18,5 +21,6 @@ const routes: Routes = [
 @NgModule({
   declarations: [PlanetsComponent, PlanetComponent],
   imports: [CommonModule, RouterModule.forChild(routes)],
+  providers: [PlanetsApi]
 })
 export class PlanetsModule {}
