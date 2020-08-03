f5ab0f2b995e59bb36e1576069dc8f7059bbf6a5
Sebastian Florian <rebardesigndk@gmail.com>
2020-08-03 23:42:07 +0200

    feature(modules): Add list and detail module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ListModule } from './modules/list/list.module';
+import { DetailModule } from './modules/detail/detail.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    ListModule,
+    DetailModule
   ],
   providers: [],
   bootstrap: [AppComponent]
