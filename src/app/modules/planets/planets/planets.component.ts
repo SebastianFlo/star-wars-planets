2e6c230b9806826611d26cc25163b68726c37a4b
Sebastian Florian <rebardesigndk@gmail.com>
2020-08-05 11:08:26 +0200

    feature(Planets): Optimistic data in Planet

--- src/app/modules/planets/planets/planets.component.ts
+++ src/app/modules/planets/planets/planets.component.ts
@@ -6,8 +6,9 @@ import { Store, select } from '@ngrx/store';
 import { PlanetState } from 'src/app/data/modules/planets/reducer';
 
 import { AppState } from 'src/app/data/state';
-import { getPlanets } from 'src/app/data/modules/planets/actions';
+import { getPlanets, getPlanetSuccess } from 'src/app/data/modules/planets/actions';
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-planets',
@@ -17,13 +18,19 @@ import { map } from 'rxjs/operators';
 export class PlanetsComponent implements OnInit {
   planets$: Observable<Planet[]>;
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<AppState>, private router: Router) { }
 
   ngOnInit(): void {
     this.planets$ = this.store.pipe(select('planets'), map(state => state.planets));
 
     // TODO: Some caching
     this.store.dispatch(getPlanets());
+  }
+
+  goToPlanet(planet: Planet) {
+    // trigger temporary planet data
+    this.store.dispatch(getPlanetSuccess({ active: planet}));
+    this.router.navigate(['planets', planet.id]);
   }
 
 }
